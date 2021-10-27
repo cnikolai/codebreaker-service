775e55758e88adea1539ae1aeca0f3d2f5b1fd34
Cynthia Nikolai <cynthianikolai@gmail.com>
2021-10-27 19:11:14 -0400

    Game Entity class.

--- src/main/java/edu/cnm/deepdive/codebreaker/model/entity/Game.java
+++ src/main/java/edu/cnm/deepdive/codebreaker/model/entity/Game.java
@@ -4,23 +4,88 @@ import java.util.Date;
 import java.util.UUID;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.ManyToAny;
 
+@SuppressWarnings("JpaDataSourceORMInspection")
 @Entity
 public class Game {
 
+  @Id
+  @GeneratedValue //automatically creates this
+  @Column(name = "game_id", updatable = false, columnDefinition = "UUID")//there is no UUID type in general in database, but telling hibernate that trust me, there is a UUID type in database
   private UUID id;
 
-  private UUID externalKey;
+  @Column(nullable = false, updatable = false, columnDefinition = "UUID", unique = true)
+  private UUID externalKey = UUID.randomUUID();
 
+  @ManyToOne(optional = false, fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id", nullable = false, updatable = false)
   private User user; //thing that we look up with the foreign key, not foreggn key itself
 
+  @CreationTimestamp//hibernate timestamp
+  @Temporal(TemporalType.TIMESTAMP)//when only attribute that going to specify, don't need value=Temp...
+  @Column(nullable = false, updatable = false)
   private Date created;
 
+  @Column(nullable = false, updatable = false, length = 255)
   private String pool;
 
+  @Column(nullable = false, updatable = false)
   private int length;
 
+  @Column(nullable = false, updatable = false, length = 20)
   private String text;
 
+  public UUID getId() {
+    return id;
+  }
+
+  public UUID getExternalKey() {
+    return externalKey;
+  }
+
+  public Date getCreated() {
+    return created;
+  }
+
+  public User getUser() {
+    return user;
+  }
+
+  public void setUser(User user) {
+    this.user = user;
+  }
+
+  public String getPool() {
+    return pool;
+  }
+
+  public void setPool(String pool) {
+    this.pool = pool;
+  }
+
+  public int getLength() {
+    return length;
+  }
+
+  public void setLength(int length) {
+    this.length = length;
+  }
+
+  public String getText() {
+    return text;
+  }
+
+  public void setText(String text) {
+    this.text = text;
+  }
 }
--- src/main/java/edu/cnm/deepdive/codebreaker/model/entity/User.java
+++ src/main/java/edu/cnm/deepdive/codebreaker/model/entity/User.java
@@ -1,12 +1,18 @@
 package edu.cnm.deepdive.codebreaker.model.entity;
 
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.UUID;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Index;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -41,6 +47,11 @@ public class User {
   @Column(nullable = false, updatable = true, unique = true, length = 100)
   private String displayName;
 
+  @OneToMany(mappedBy = "user", fetch = FetchType.LAZY,
+      cascade = CascadeType.ALL, orphanRemoval = true) //mappedBy = specify the field where this relationship was defined (the field where defined the foreign key and ...)
+  @OrderBy("created DESC") //when you do a query, here is the order we would like you to use.
+  private final List<Game> games = new LinkedList<>(); //when retrieve a user, looking up a list of games by that user
+
   public UUID getId() {
     return id;
   }
@@ -69,4 +80,7 @@ public class User {
     this.displayName = displayName;
   }
 
+  public List<Game> getGames() {
+    return games;
+  }
 }
